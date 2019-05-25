435332a4cfb505be78af08023cbf5c887800b4f5
kts of kettek (Tyger) <kettek1@kettek.net>
2019-05-25 07:21:30 -0700

    Implement  configuration from environment

--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"flag"
+	"strconv"
 	"os"
 	"path"
 	"path/filepath"
@@ -21,23 +22,44 @@ func main() {
 		fmt.Print(err)
 		return
 	}
-	sharePath := path.Join(filepath.Dir(filepath.Dir(binaryPath)), "share", "go-learn-it")
 
-	addressPtr := flag.String("address", "", "HTTP listen address")
-	portPtr := flag.Int("port", 8888, "HTTP listen port")
-	sharepathPtr := flag.String("sharepath", sharePath, "Path to the built-in go-learn-it static and template data")
-	datapathPtr := flag.String("datapath", "", "Path to external go-learn-it static and template data")
-	curriculumPtr := flag.String("curriculum", "curriculum.json", "Path to the curriculum data file located in either basepath or datapath")
+	address := ""
+	port := 8888
+	sharepath := path.Join(filepath.Dir(filepath.Dir(binaryPath)), "share", "go-learn-it")
+	datapath := ""
+	curriculum := "curriculum.json"
+
+	if addressEnv, exists := os.LookupEnv("GLI_ADDRESS"); exists {
+		address = addressEnv
+	}
+	if portEnv, exists := os.LookupEnv("GLI_PORT"); exists {
+		port, _ = strconv.Atoi(portEnv)
+	}
+	if sharepathEnv, exists := os.LookupEnv("GLI_SHAREPATH"); exists {
+		sharepath = sharepathEnv
+	}
+	if datapathEnv, exists := os.LookupEnv("GLI_DATAPATH"); exists {
+		datapath = datapathEnv
+	}
+	if curriculumEnv, exists := os.LookupEnv("GLI_CURRICULUM"); exists {
+		curriculum = curriculumEnv
+	}
+
+	flag.StringVar(&address, "address", address, "HTTP listen address - GLI_ADDRESS")
+	flag.IntVar(&port, "port", port, "HTTP listen port - GLI_PORT")
+	flag.StringVar(&sharepath, "sharepath", sharepath, "Path to the built-in go-learn-it static and template data - GLI_SHAREPATH")
+	flag.StringVar(&datapath, "datapath", datapath, "Path to external go-learn-it static and template data - GLI_DATAPATH")
+	flag.StringVar(&curriculum, "curriculum", curriculum, "Filename of the curriculum data file located in either basepath or datapath - GLI_CURRICULUM")
 
 	flag.Parse()
 
-	if len(*sharepathPtr) > 0 {
-		courseMaster.multiPath.AddPath(*sharepathPtr, multipath.FirstPriority)
-		staticMultiPath.AddPath(path.Join(*sharepathPtr, "static"), multipath.FirstPriority)
+	if len(sharepath) > 0 {
+		courseMaster.multiPath.AddPath(sharepath, multipath.FirstPriority)
+		staticMultiPath.AddPath(path.Join(sharepath, "static"), multipath.FirstPriority)
 	}
-	if len(*datapathPtr) > 0 {
-		courseMaster.multiPath.AddPath(*datapathPtr, multipath.FirstPriority)
-		staticMultiPath.AddPath(path.Join(*datapathPtr, "static"), multipath.FirstPriority)
+	if len(datapath) > 0 {
+		courseMaster.multiPath.AddPath(datapath, multipath.FirstPriority)
+		staticMultiPath.AddPath(path.Join(datapath, "static"), multipath.FirstPriority)
 	}
 
 	//
@@ -45,7 +67,7 @@ func main() {
 
 	// Setup our HTTP server.
 	fs := multipathFileSystem{http.Dir(""), &staticMultiPath}
-	if courseMaster.Curriculum, err = CurriculumFromMultiPath(courseMaster.multiPath, *curriculumPtr); err != nil {
+	if courseMaster.Curriculum, err = CurriculumFromMultiPath(courseMaster.multiPath, curriculum); err != nil {
 		fmt.Print(err)
 		return
 	}
@@ -57,8 +79,8 @@ func main() {
 	http.HandleFunc("/api/", courseMaster.handleAPI)
 	http.HandleFunc("/", courseMaster.handleHTTP)
 
-	http.ListenAndServe(fmt.Sprintf("%s:%d", *addressPtr, *portPtr), nil)
-	fmt.Printf("Now listening on %s:%d\n", *addressPtr, *portPtr)
+	http.ListenAndServe(fmt.Sprintf("%s:%d", address, port), nil)
+	fmt.Printf("Now listening on %s:%d\n", address, port)
 }
 
 type multipathFileSystem struct {
