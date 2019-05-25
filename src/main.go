1f56adf9f640990d1069ca15988003bf453c19ab
kts of kettek (Tyger) <kettek1@kettek.net>
2019-05-25 16:04:41 -0700

    Adjust main comments

--- src/main.go
+++ src/main.go
@@ -23,12 +23,14 @@ func main() {
 		return
 	}
 
+	// Default run-time variables
 	address := ""
 	port := 8888
 	sharepath := path.Join(filepath.Dir(filepath.Dir(binaryPath)), "share", "go-learn-it")
 	datapath := ""
 	curriculum := "curriculum.json"
 
+	// Merge environment with run-time
 	if addressEnv, exists := os.LookupEnv("GLI_ADDRESS"); exists {
 		address = addressEnv
 	}
@@ -45,6 +47,7 @@ func main() {
 		curriculum = curriculumEnv
 	}
 
+	// Override run-time/environment with flags
 	flag.StringVar(&address, "address", address, "HTTP listen address - GLI_ADDRESS")
 	flag.IntVar(&port, "port", port, "HTTP listen port - GLI_PORT")
 	flag.StringVar(&sharepath, "sharepath", sharepath, "Path to the built-in go-learn-it static and template data - GLI_SHAREPATH")
@@ -53,20 +56,21 @@ func main() {
 
 	flag.Parse()
 
+	// Add sharepath to the multipath if it is set.
 	if len(sharepath) > 0 {
 		courseMaster.multiPath.AddPath(sharepath, multipath.FirstPriority)
 		staticMultiPath.AddPath(path.Join(sharepath, "static"), multipath.FirstPriority)
 	}
+	// Add datapath to the multipath if it is set.
 	if len(datapath) > 0 {
 		courseMaster.multiPath.AddPath(datapath, multipath.FirstPriority)
 		staticMultiPath.AddPath(path.Join(datapath, "static"), multipath.FirstPriority)
 	}
 
-	//
+	// Okay, we're good to go!
 	fmt.Println("Starting go-learn-it")
 
-	// Setup our HTTP server.
-	fs := multipathFileSystem{http.Dir(""), &staticMultiPath}
+	// Setup the CourseMaster.
 	if courseMaster.Curriculum, err = CurriculumFromMultiPath(courseMaster.multiPath, curriculum); err != nil {
 		fmt.Print(err)
 		return
@@ -75,6 +79,8 @@ func main() {
 		fmt.Print(err)
 		return
 	}
+	// Setup our HTTP server.
+	fs := multipathFileSystem{http.Dir(""), &staticMultiPath}
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(fs)))
 	http.HandleFunc("/api/", courseMaster.handleAPI)
 	http.HandleFunc("/", courseMaster.handleHTTP)
@@ -88,6 +94,7 @@ type multipathFileSystem struct {
 	mpath *multipath.Multipath
 }
 
+// Open provides the multipath opening for our multipath HTTP FileSystem
 func (fs multipathFileSystem) Open(name string) (http.File, error) {
 	file, err := fs.mpath.Open(name)
 	return file, err
